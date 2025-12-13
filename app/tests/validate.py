089a47bd3fbd86a6addd3b510fd5e74c4668e545
prashant1926misra <prashantnau2611@gmail.com>
2025-12-13 16:10:10 +0530

    Add healthcheck job to CI and implement validation tests

--- app/tests/validate.py
+++ app/tests/validate.py
@@ -1,1 +1,18 @@
+import requests
 
+def url_ok(url):
+    try:
+        r = requests.head(url, timeout=5)
+        return r.status_code == 200
+    except Exception as e:
+        print("Error reaching app:", e)
+        return False
+
+if __name__ == "__main__":
+    url = "http://43.205.211.118:5000/health"
+    if url_ok(url):
+        print("App is healthy")
+        exit(0)
+    else:
+        print("App is not healthy")
+        exit(1)
