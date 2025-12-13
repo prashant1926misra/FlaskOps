789f9e10ae6f97fb01475893f2ddb15aae222ff8
prashant1926misra <prashantnau2611@gmail.com>
2025-12-13 13:42:53 +0530

    Set debug=False in app.py

--- app/app.py
+++ app/app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route('/health')
 def health():
-    return "OK"
+    return "OK Master!"
 
 @app.route('/hello')
 def hello():
     return "Hello Master!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
